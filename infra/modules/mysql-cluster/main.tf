d0f5c976549dc25b3bbe1c70e58c63951e891ceb
ArthurSampaio13 <arthur.sampaio@upe.br>
2026-09-02 15:43:30 -0300

    ci: collect mysql diagnostics when make infra fails and give a fresh cluster 25 minutes

--- infra/modules/mysql-cluster/main.tf
+++ infra/modules/mysql-cluster/main.tf
@@ -151,9 +151,9 @@ resource "kubectl_manifest" "mysql_podmonitor" {
 resource "null_resource" "wait_online" {
   provisioner "local-exec" {
     command = <<-EOT
-      kubectl --kubeconfig ${var.kubeconfig_path} -n mysql wait innodbcluster/mysql --for=jsonpath='{.status.cluster.status}'=ONLINE --timeout=900s &&
+      kubectl --kubeconfig ${var.kubeconfig_path} -n mysql wait innodbcluster/mysql --for=jsonpath='{.status.cluster.status}'=ONLINE --timeout=1500s &&
       until kubectl --kubeconfig ${var.kubeconfig_path} -n mysql get deploy mysql-router >/dev/null 2>&1; do sleep 5; done &&
-      kubectl --kubeconfig ${var.kubeconfig_path} -n mysql wait deploy/mysql-router --for=condition=Available --timeout=900s
+      kubectl --kubeconfig ${var.kubeconfig_path} -n mysql wait deploy/mysql-router --for=condition=Available --timeout=1500s
     EOT
   }
   depends_on = [kubectl_manifest.innodb_cluster]
